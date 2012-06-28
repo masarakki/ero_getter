240f0aadff65391e7c82752b73be42acd5e9726c
masarakki <masaki@hisme.net>
2012-06-29 05:30:08 +0900

    add mink_channel

---
+++ lib/downloader/mink_channel.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+class MinkChannel < EroGetter::Base
+
+  name 'みんくちゃんねる'
+  url %r{http://minkch.com/archives/(\d+).html}
+
+  target ".gold > a > img" do |path|
+    path.parent[:href] if path.parent[:href] =~ /jpe?g|gif|png/
+  end
+
+  sub_directory do
+    url.match(/(\d+)\.html/)[1]
+  end
+
+  filename {|attr| "%04d%s" % [attr[:index], attr[:ext]] }
+
+end
---
+++ spec/downloader/mink_channel_spec.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe MinkChannel do
+  let(:url) { "http://minkch.com/archives/51930285.html" }
+  subject { @dl }
+  before do
+    EroGetter.stub(:mkdir)
+    @dl = MinkChannel.new(url)
+    fake(:get, url, 'mink_channel/sample.html')
+  end
+  its(:title) { should == '顔出しＮＧの現役女子大生グラドルが手ブラでおっぱい隠して話題に - みんくちゃんねる' }
+  its(:sub_directory) { should == '51930285' }
+  its('targets.count') { should == 10 }
+end

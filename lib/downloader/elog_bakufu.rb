c41057e7a2632fbeb3d7c87c303b3994286e2c90
masarakki <masaki@hisme.net>
2012-07-01 04:18:07 +0900

    add elog_bakufu

---
+++ lib/downloader/elog_bakufu.rb
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+class ElogBakufu < EroGetter::Base
+
+  name 'エログ幕府'
+  url %r{http://blog.livedoor.jp/samplems-bakufu/archives/51351906.html}
+
+  target ".article-body-inner img" do |path|
+    if path.parent.name == "a" && path.parent[:href] =~ /jpe?g|gif|png$/
+      path.parent[:href]
+    else
+      path[:src]
+    end
+  end
+
+  sub_directory do
+    url.match(/(\d+).html/)[1]
+  end
+
+  filename { |attr| "%04d%s" % [attr[:index], attr[:ext]] }
+
+end
---
+++ spec/downloader/elog_bakufu_spec.rb
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe ElogBakufu do
+  let(:url) { 'http://blog.livedoor.jp/samplems-bakufu/archives/51351906.html' }
+  subject { @dl }
+
+  before do
+    EroGetter.stub(:mkdir)
+    @dl = ElogBakufu.new(url)
+    fake(:get, url, 'elog_bakufu/sample.html')
+  end
+  its(:name) { should == 'エログ幕府' }
+  its(:title) { should == '制服女子がＭ字開脚してるエロ画像 part2 - お宝エログ幕府' }
+  its(:base_dir) { should == 'elog_bakufu' }
+  its(:sub_directory) { should == '51351906' }
+  its("targets.count") { should == 40 }
+end
